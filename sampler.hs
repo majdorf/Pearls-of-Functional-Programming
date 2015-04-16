94374ce74dcfa5f9d91e0a2599022c00c817823a
sten <majdof@gmail.com>
2015-04-16 23:24:38 +0200

    added k element of merge two sorted lists

--- sampler.hs
+++ sampler.hs
@@ -1,3 +1,5 @@
+import Data.Array
+
 tails :: [a] -> [[a]]
 tails [] = []
 tails (x:xs) = (x:xs):tails xs
@@ -66,3 +68,38 @@ invert2 f z = find (0,m)  (n,0) f z
 				   
 f0::Int->Int->Int
 f0 x y = (2^y) * ( 2 * x + 1) - 1
+
+--smallest k (xs,ys) = union (xs,ys) !! k
+smallest::Ord a =>Int->([a],[a])->a
+smallest k ([],ws) = ws !! k
+smallest k (zs,[]) = zs !! k
+smallest k (zs,ws) = 
+	case (a < b, k <= p + q) of
+		(True, True) -> smallest k (zs,us)
+		(True, False)-> smallest (k-p-1) (ys,ws)
+		(False,True) -> smallest k (xs,ws)
+		(False, False) -> smallest (k - q - 1) (zs,vs)
+	where 
+		p = (length zs) `div` 2
+		q = (length ws) `div` 2
+		(xs,a:ys) = splitAt p zs
+		(us,b:vs) = splitAt q ws
+
+
+	
+		
+smallestA::Ord a =>Int->(Array Int a, Array Int a)->a
+smallestA k (xa,ya) = searchA k (0,m+1) (0,n+1)
+	where 
+		(0,m) = bounds xa
+		(0,n) = bounds ya
+		searchA k (lx,rx) (ly,ry)
+			| lx == rx  = ya ! k
+			| ly == ry  = xa ! k
+			| otherwise = case (xa ! mx < ya! my, k <= mx+my ) of
+				(True,True)-> searchA k (lx,rx) (ly,my)
+				(True,False)-> searchA (k-mx-1) (mx,rx) (ly,ry)
+				(False,True)-> searchA k (lx,mx) (ly,ry)
+				(False,False)-> searchA (k - my -1) (lx,rx) (my,ry)
+				where mx = (lx +rx `div` 2); my = (ly+ry) `div` 2
+
