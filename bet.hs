c424917953f566ac31bf9453415c15e1195dd134
majdorf <majdof@gmail.com>
2015-10-21 20:25:53 +0200

    test commit

---
+++ bet.hs
@@ -0,0 +1,11 @@
+import Data.List (nub, sort)  
+
+transform:: Ord a=> [a]->([a],Int)
+transform xs = (map last xss, position xs xss)
+	where xss = sort (rots xs)
+	
+position xs xss = length (takeWhile (/=xs) xss)
+
+rots::[a]->[[a]]
+rots xs = take (length xs) (iterate lrot xs)
+	where lrot (x:xs) = xs++[x]
