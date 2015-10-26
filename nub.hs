d5b5a51706cfd37483b4665f210e8cb554c6d9ba
majdorf <majdof@gmail.com>
2015-10-26 22:52:27 +0100

    added nub2

--- nub.hs
+++ nub.hs
@@ -1,4 +1,5 @@
 import Data.List( (\\))
+import Data.Set
 
 nub = hub []
 hub ws [] = []
@@ -9,3 +10,22 @@ hub ws (x:xs) = case (x `elem` xs, x `elem` ws) of
 					(True,True)   -> hub ws xs
 				where
 					(us,vs) = span (<x) ws
+					
+					
+					
+					
+					
+					
+nub2 = hub empty.preprocess
+preprocess xs = zip xs (tail (scanr insert empty xs))
+hub ws [] = []
+hub ws ((x,xs):xss) = 
+	case (member x xs, member x ws) of
+		(False, False) -> eus ++ [x] ++ hub empty yss
+		(False, True) -> eus ++ [x] ++ hub vs yss
+		(True, False) -> hub (insert x us) xss
+		(True,True) -> hub ws xss
+	where 
+		(us,vs) = split x ws
+		eus = elems us
+		yss = [(x,xs)| (x,xs) <- xss, not (member x us)]
