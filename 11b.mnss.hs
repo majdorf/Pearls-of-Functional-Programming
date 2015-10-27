abc9ba770e95576a83bf462fabdf52ca31f68fae
majdorf <majdorf@gmail.com>
2015-10-27 22:56:20 +0100

    fix

--- 11b.mnss.hs
+++ 11b.mnss.hs
@@ -2,11 +2,11 @@
 mnss::[Int]->Int
 mnss = maximum.map sum.nonsegs
 
-markings::[a]->[(a,Bool)]
+markings::[a]->[[(a,Bool)]]
 markings xs = [ zip xs bs | bs <- booleans (length xs)]
 
 booleans 0 = [[]]
-booleans (n+1) = [b:bs | b<-[True,False], bs <- booleans n]
+booleans n = [b:bs | b <- [True,False], bs <- booleans (n - 1)]
 
 nonsegs::[a] ->[[a]]
 nonsegs = extract.filter nonseg.markings
@@ -14,9 +14,9 @@ nonsegs = extract.filter nonseg.markings
 extract::[[(a,Bool)]] -> [[a]]
 extract = map( map fst.filter snd)
 
-data State = E | S | M | N
+data StateS = E | S | M | N
+	deriving (Eq)
 --F*T+F+T(T + F)*
-nonseg = (==N).foldl step E.map snd
 step E  False = E
 step E  True  = S
 step S  False = M
@@ -26,8 +26,13 @@ step M True = N
 step N False = N
 step N True = N
 
+
+nonseg::[(a,Bool)] -> Bool
+nonseg = (N == ).(foldl step E).map snd
+
+
 --second solution
-mnss2 xs = fourth (foldl h (start (take 3 xs)) (drop 3 xs))
-start [x,y,z] = (0, max [x+y+z,y+z,z], max [x, x+y, y], x + z)
+--mnss2 xs = fourth (foldl h (start (take 3 xs)) (drop 3 xs))
+--start [x,y,z] = (0, max [x+y+z,y+z,z], max [x, x+y, y], x + z)
 
 
