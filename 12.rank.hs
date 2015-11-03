a9372382a308c915be4508bed39b866c99650cba
majdorf <majdorf@gmail.com>
2015-11-03 22:50:05 +0100

    in progress

--- 12.rank.hs
+++ 12.rank.hs
@@ -13,18 +13,18 @@ pass (e@(x,y):xys) xss = step xys [] [x] [] xss
 
 
 										 
---ranktails::Ord a => [a]->[Int]
---ranktails xs = (resort n.concat.label.applyUntil isperm (repartitions n).psort.zip [0..]) xs
---	where
---		n = length xs
---		resort n = elems.array (0,n-1)
---		label iss = zipWith tag iss (scanl (+) 0 (map length iss))
---		tag is j = [(i,j) | i <- is]
---		repartitions n  = map (repartition n) (iterate (*2) 1)
---		repartition n k iss = concatMap (psort.map install) iss
---			where
---				install i = (i, if j<n then k + a!j else n - i - 1)
---				a = array (0,n-1) (concat (label iss))
+ranktails::Ord a => [a]->[Int]
+ranktails xs = (resort n.concat.label.applyUntil isperm (repartitions n).psort.zip [0..]) xs
+	where
+		n = length xs
+		resort n = elems.array (0,n-1)
+		label iss = zipWith tag iss (scanl (+) 0 (map length iss))
+		tag is j = [(i,j) | i <- is]
+		repartitions n  = map (repartition n) (iterate (*2) 1)
+		repartition n k iss = concatMap (psort.map install) iss
+			where
+				install i = (i, if i + k <n then k + a! (i+ k) else n - i - 1)
+				a = array (0,n-1) (concat (label iss))
 
 applyUntil::(a->Bool)->[a->a]->a->a
 applyUntil p (f:fs) x = if p x then x else applyUntil p fs ( f x)
@@ -35,7 +35,7 @@ isperm is = and ( elems (accumArray (||) False (0, n - 1) (zip is (repeat True))
 		n = length is
 
 
---rank::Ord a =>[a]->[Int]
+rank::Ord a =>[a]->[Int]
 rank  = resort.concat.label.psort.zip [0..] 
 
 resort::[(Int,Int)]->[Int]
@@ -48,6 +48,7 @@ tag xs k = [(x,k) | x <- xs]
 partition::Ord a =>[a]->[[Int]]
 partition = psort.zip [0..]
 	
+ranktails0::Ord a =>[a]->[Int]
 ranktails0 = applyUntil isperm rerankings.rank
 rerankings = map rerank (iterate (*2) 1)
 rerank k rs = rs `rzip` shiftBy k rs
