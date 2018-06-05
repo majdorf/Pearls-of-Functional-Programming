0f57c5beb98236d7e4a8acb4ca1965efbc2d776d
majdorf <majdorf@gmail.com>
2018-06-05 17:57:26 +0200

    celebrity improvements

--- 9.rem_celebrities.hs
+++ 9.rem_celebrities.hs
@@ -1,3 +1,23 @@
 
 subseqs [ ] = [[ ]]
 subseqs (x : xs) = map (x :) (subseqs xs) ++ subseqs xs
+
+
+nonmember p cs = and [p knows c ∧ not (c knows p) | c ← cs]
+member p ps cs = and [x knows p ∧ (p knows x ⇔ x in cs | x ←ps]
+
+ccliques [] = [[]]
+ccliques (p:ps) = map (p:) (filter (member p ps) css) ++
+				  filter (nonmember p) css
+				  where css = ccliques ps
+				  
+--linear time algorithm					
+cclique' = foldr op []
+op p cs | null cs = [p]
+		| not (p knows c) = [p]
+		| not (c knows p) = cs
+		| otherwise = p:cs
+		where c = head cs
+		
+		
+--not (null (cclique ps)) ⇒ cclique ps = cclique' ps
